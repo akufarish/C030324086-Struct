e57a1041c91be140b9e2f2a6b49efd84cc64e9db
akufarish <risfarishk@gmail.com>
2025-05-28 20:29:09 +0800

    pengujian function init, push, pop, dan display

--- C030324086-Stack.cpp
+++ C030324086-Stack.cpp
@@ -19,10 +19,20 @@ void Pop(Stack *S, itemType *x);
 void Push(Stack *S, itemType x);
 void display(Stack *S);
 
-
 int main(int argc, char const *argv[])
 {
-    cout << "Hello World!" << endl;
+    Stack S;
+    itemType x;
+
+    InitializeStack(&S);
+
+    Push(&S, 2);
+    Push(&S, 5);
+    Push(&S, 9);
+    Push(&S, 1);
+    Push(&S, 3);
+    Pop(&S, &x);
+    display(&S);
     
     return 0;
 }
