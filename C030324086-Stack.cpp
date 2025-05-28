86772232f2c279efafc779bdc292ac5e28fa3118
akufarish <risfarishk@gmail.com>
2025-05-28 20:20:24 +0800

    inisalisasi function display

--- C030324086-Stack.cpp
+++ C030324086-Stack.cpp
@@ -17,6 +17,8 @@ int Full(Stack *S);
 int Empty(Stack *S);
 void Pop(Stack *S, itemType *x);
 void Push(Stack *S, itemType x);
+void display(Stack *S);
+
 
 int main(int argc, char const *argv[])
 {
