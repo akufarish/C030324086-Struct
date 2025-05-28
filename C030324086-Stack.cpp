da577b4c297def9e6b30362f16fb1f7e0743e5ba
akufarish <risfarishk@gmail.com>
2025-05-28 20:24:41 +0800

    fix duplikat function push

--- C030324086-Stack.cpp
+++ C030324086-Stack.cpp
@@ -17,7 +17,6 @@ int Full(Stack *S);
 int Empty(Stack *S);
 void Pop(Stack *S, itemType *x);
 void Push(Stack *S, itemType x);
-void display(Stack *S);
 
 
 int main(int argc, char const *argv[])
@@ -48,15 +47,6 @@ void Pop(Stack *S, itemType *x) {
         *x = S->Item[S->Count];
     }
     
-}
-
-void Push(Stack *S, itemType x) {
-    if (S->Count == MAXSTACK) {
-        cout << "Stack sudah penuh" << endl;
-    } else {
-        S->Item[S->Count] = x;
-        ++(S->Count);
-    }
 }
 
 void Push(Stack *S, itemType x) {
